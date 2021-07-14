ad73866bdbae428eba1397dd710aec662a4f6cd7
Wesley <wesleygurgel27@gmail.com>
2021-07-14 11:00:47 -0300

    (DEV): Criação de Models + Views + URL

--- bank_system/settings.py
+++ bank_system/settings.py
@@ -130,10 +130,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.joinh(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.joinh(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- core/models.py
+++ core/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Base(models.Model):
+    criados = models.DateField('Criação', auto_now_add=True)
+    modificado = models.DateField('Atualização', auto_now=True)
+    ativo = models.BooleanField('Ativo?', default=True)
+
+    class Meta:
+        abstract = True
+
+
+class Conta(Base):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    proprietario = models.CharField('Proprietário', max_length=100)
+    credito = models.DecimalField('Crédito', max_digits=3, decimal_places=2)
+    saldo = models.FloatField(default=0.0)
+
+    class Meta:
+        verbose_name = 'Conta'
+
+    def __str__(self):
+        return f'Conta: {self.usuario}\nProprietário: {self.proprietario}\nSaldo: {self.saldo}'
+
+
+
+
+
+
+
