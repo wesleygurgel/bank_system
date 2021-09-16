badc02eecd2f7ae0d019a077f5e927f5e4a95685
Wesley Gurgel <wesley.gurgel@resale.com.br>
2021-09-16 14:06:27 -0300

    fix(commit-msg): lets go boys: fix/SW-2820

--- commit-msg.py
+++ commit-msg.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3.8
 
-import sys, re
+import sys, re, getpass
+
+rules = """
+# Por favor, digite uma mensagem de commit respeitando o padrão!
+# Exemplo: feat(done_something): created gambiarra: feature/SW-2820
+# Atente para:
+#   - Usar apenas letras minúsculas, excessão para o numero do card -> (SW-2820).
+#   - Não usar espaços em excesso.
+#   - Presença de caracteres como ":" e "/" são importantes! 
+"""
 
 
 def main():
@@ -8,18 +17,18 @@ def main():
         lines = fp.readlines()
         for line in lines:
             if not line_valid(line):
-                print(
-                    'Por favor, digite uma mensagem de commit respeitando o padrão\nExemplo:  fix(texto_qualquer): my '
-                    'changes are this\nAtente para o limite de 50 caracteres na descrição!')
+                print(rules)
                 sys.exit(1)
             else:
-                print('Commit realizado respeitando o padrão!')
+                print(f'Commit realizado respeitando o padrão!\nVocê é o cara {getpass.getuser()}')
                 sys.exit(0)
 
 
 def line_valid(text):
     # return re.match("#\s?\d{,10}\s?-\s?(\w|\s){,49}", text)
-    return re.match("(feat|dev|fix|perf|refactory|build|ci|docs|style|test|chore|env)\([^)]*\):(\w|\s){,49}", text)
+    return re.match(
+        "(feat|dev|fix|perf|refactory|build|ci|docs|style|test|chore|env)\((\s|[a-z]|-|_)*\):(\s|[a-z]|-|_)*:\s?\w{,7}?/?[A-Z]{2}-\d{4}",
+        text)
 
 
 if __name__ == '__main__':
