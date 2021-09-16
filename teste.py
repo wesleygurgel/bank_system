8ca51276d89ca166cbd9f79953dabff3c91cdcd3
Wesley Gurgel <wesley.gurgel@resale.com.br>
2021-09-16 14:09:37 -0300

    feat(commit-msg): lets go boys: feature/SW-2820

--- teste.py
+++ teste.py
@@ -2,6 +2,6 @@ import re
 
 string = "#7 - oi"
 
-string2 = "# 8 - eaieeeqweqweqwqweqweqawq"
+string2 = "# 8 - eaieeeqweqweqwqweqweqawqewq"
 
 print(re.match("#\s?\d{1,3}\s?-\s?(\w|\s){,49}", string))
