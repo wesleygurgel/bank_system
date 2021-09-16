ee2aae07926304ae9fb81cf5462ad6c8b3b3ebc6
Wesley Gurgel <wesley.gurgel@resale.com.br>
2021-09-16 16:03:45 -0300

    a

--- teste.py
+++ teste.py
@@ -2,6 +2,10 @@ import re
 
 string = "#7 - oi"
 
-string2 = "# 8 - eaieeeqweqwqqwewqweqweqwqweqweqawqewq"
+<<<<<<< Updated upstream
+string2 = "# 8 - qwewqqweq"
+=======
+string2 = "# 8 - eaiqweqqweqww"
+>>>>>>> Stashed changes
 
 print(re.match("#\s?\d{1,3}\s?-\s?(\w|\s){,49}", string))
