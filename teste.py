e7572e6de951808c9a38d1bef8d1aa66fd7276d5
Wesley Gurgel <wesley.gurgel@resale.com.br>
2021-09-17 11:49:21 -0300

    feat(hello): commitando aqui amigo: feature/SW-2820

--- teste.py
+++ teste.py
@@ -2,10 +2,9 @@ import re
 
 string = "#7 - oi"
 
-<<<<<<< Updated upstream
-string2 = "# 8 - qwewqqweq"
-=======
+
+string2 = "# 8 - qwewqqweqweqweqweqweqwqw"
 string2 = "# 8 - eaiqweqqweqww"
->>>>>>> Stashed changes
+
 
 print(re.match("#\s?\d{1,3}\s?-\s?(\w|\s){,49}", string))
